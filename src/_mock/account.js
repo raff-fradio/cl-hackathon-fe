29fbadf7debcdb8689530798beac798eb2bbe989
Jason Yapri <jason.yapri@gmail.com>
2023-06-09 21:03:34 +0800

    Changed logo, DP, Name

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,9 +1,9 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
-  photoURL: '/assets/images/avatars/avatar_default.jpg',
+  displayName: 'Raffael Fradio',
+  email: 'raff.fradio@gmail.com ',
+  photoURL: '/assets/images/avatars/avatar_12.jpg',
 };
 
 export default account;
